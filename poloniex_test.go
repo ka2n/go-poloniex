d2a8b2e0983e411b9bc5ed0c15296c418374f31f
Katsuma Ito <katsumai@gmail.com>
2017-06-21 18:01:17 +0900

    Fix test for private API

--- poloniex_test.go
+++ poloniex_test.go
@@ -2,13 +2,17 @@ package poloniex
 
 import (
 	"context"
+	"io/ioutil"
 	"log"
-	"os"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
 	"testing"
 )
 
+var logger = log.New(ioutil.Discard, "", 0)
+
 func TestClient_GetCurrencies(t *testing.T) {
-	logger := log.New(os.Stderr, "", log.LstdFlags)
 	client, err := New("", "", "", logger)
 	if err != nil {
 		t.Fatal(err)
@@ -25,11 +29,11 @@ func TestClient_GetCurrencies(t *testing.T) {
 }
 
 func TestClient_GetBalances(t *testing.T) {
-	logger := log.New(os.Stderr, "", log.LstdFlags)
-	client, err := NewPrivateClient(logger)
-	if err != nil {
-		t.Fatal(err)
-	}
+	server, client := newMockedClient([]mockResponse{
+		{Action: "returnBalances", PrivateAPI: true, BodyFile: "testdata/returnBalances.json"},
+	})
+	defer server.Close()
+
 	ctx := context.Background()
 	ret, err := client.GetBalances(ctx)
 	if err != nil {
@@ -39,14 +43,23 @@ func TestClient_GetBalances(t *testing.T) {
 	if ret == nil {
 		t.Fatal("ret should be non nil")
 	}
+
+	expect := &Balances{map[string]float64{
+		"LTC": 1000.11111111,
+		"BTC": 0.000000003,
+	}}
+
+	if !reflect.DeepEqual(expect, ret) {
+		t.Errorf("expect: %+v, got: %+v", expect, ret)
+	}
 }
 
 func TestClient_GetCompleteBalances(t *testing.T) {
-	logger := log.New(os.Stderr, "", log.LstdFlags)
-	client, err := NewPrivateClient(logger)
-	if err != nil {
-		t.Fatal(err)
-	}
+	server, client := newMockedClient([]mockResponse{
+		{Action: "returnCompleteBalances", PrivateAPI: true, BodyFile: "testdata/returnCompleteBalances.json"},
+	})
+	defer server.Close()
+
 	ctx := context.Background()
 	ret, err := client.GetCompleteBalances(ctx)
 	if err != nil {
@@ -56,8 +69,56 @@ func TestClient_GetCompleteBalances(t *testing.T) {
 	if ret == nil {
 		t.Fatal("ret should be non nil")
 	}
+
+	expect := CompleteBalances(map[string]Balance{
+		"LTC": Balance{
+			Available: "5.015",
+			OnOrders:  "1.0025",
+			BtcValue:  "0.078",
+		},
+		"NXT": Balance{
+			Available: "0",
+			OnOrders:  "0",
+			BtcValue:  "0",
+		},
+	})
+
+	if !reflect.DeepEqual(expect, ret) {
+		t.Errorf("expect: %+v, got: %+v", expect, ret)
+	}
 }
 
-func NewPrivateClient(logger *log.Logger) (*Client, error) {
-	return New("POLONIEX_KEY", "POLONIEX_SECRET", "", logger)
+type mockResponse struct {
+	Action     string
+	PrivateAPI bool
+	BodyFile   string
+	Status     int
+}
+
+func newMockedClient(mocks []mockResponse) (*httptest.Server, *Client) {
+	muxAPI := http.NewServeMux()
+	testAPIServer := httptest.NewServer(muxAPI)
+
+	public := "/public"
+	private := "/tradingApi"
+
+	for _, mock := range mocks {
+		var req string
+
+		if mock.PrivateAPI {
+			req = private
+		} else {
+			req = public
+		}
+
+		muxAPI.HandleFunc(req, func(w http.ResponseWriter, r *http.Request) {
+			if mock.Status != 0 {
+				w.WriteHeader(mock.Status)
+			}
+			http.ServeFile(w, r, mock.BodyFile)
+		})
+	}
+
+	client, _ := New("", "", testAPIServer.URL, nil)
+	return testAPIServer, client
 }
